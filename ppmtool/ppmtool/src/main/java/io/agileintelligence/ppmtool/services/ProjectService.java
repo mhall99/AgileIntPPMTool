99bc8ac2085563f0b73c2f98a6d1667d01638037
mhall99 <erinmhall61@gmail.com>
2021-11-29 16:05:47 -0600

    created services & repositories packages

---
+++ ppmtool/ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -0,0 +1,12 @@
+package io.agileintelligence.ppmtool.services;
+
+import io.agileintelligence.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+	@Autowired
+	private ProjectRepository projectRepository;
+}
