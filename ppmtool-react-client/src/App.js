8217a9aaba5f492142742ab7acf7f56ca99ebd72
mhall99 <erinmhall61@gmail.com>
2021-12-05 22:19:35 -0600

    components folder & Dashboard welcome msg

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to PPMTool.
-        </p>
-      </header>
+      <Dashboard />
     </div>
   );
 }
