880927361cb688ecac32cddd2ca040dc2a001c77
mhall99 <erinmhall61@gmail.com>
2021-12-17 16:40:04 -0600

    loaded projectTasks to state

--- ppmtool-react-client/src/actions/backlogActions.js
+++ ppmtool-react-client/src/actions/backlogActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, GET_BACKLOG } from "./types";
 
 //happy path
 export const addProjectTask =
@@ -18,3 +18,13 @@ export const addProjectTask =
       });
     }
   };
+
+export const getBacklog = (backlog_id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/backlog/${backlog_id}`);
+    dispatch({
+      type: GET_BACKLOG,
+      payload: res.data,
+    });
+  } catch (err) {}
+};
