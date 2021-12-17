d03e8bf73e93825be472927a89ddbf2fc71c525e
mhall99 <erinmhall61@gmail.com>
2021-12-17 16:58:54 -0600

    load ProjectTasks to UI (aka u can see shit now)

--- ppmtool-react-client/src/components/ProjectBoard/Backlog.js
+++ ppmtool-react-client/src/components/ProjectBoard/Backlog.js
@@ -3,6 +3,12 @@ import ProjectTask from "./ProjectTasks/ProjectTask";
 
 class Backlog extends Component {
   render() {
+    const { project_tasks_prop } = this.props;
+
+    const tasks = project_tasks_prop.map((project_task) => (
+      <ProjectTask key={project_task.id} project_task={project_task} />
+    ));
+
     return (
       <div className="container">
         <div className="row">
@@ -12,6 +18,12 @@ class Backlog extends Component {
                 <h3>TO DO</h3>
               </div>
             </div>
+            {tasks}
+
+            {
+              //Project tasks here
+            }
+
             <ProjectTask />
           </div>
           <div className="col-md-4">
