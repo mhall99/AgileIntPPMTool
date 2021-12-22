a9c76ee3643eebf93ebd7c6461d5ad42b56d807b
mhall99 <erinmhall61@gmail.com>
2021-12-22 11:54:54 -0600

    dupe User exception handling

--- ppmtool/ppmtool/src/main/java/io/agileintelligence/ppmtool/services/UserService.java
+++ ppmtool/ppmtool/src/main/java/io/agileintelligence/ppmtool/services/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import io.agileintelligence.ppmtool.domain.User;
+import io.agileintelligence.ppmtool.exceptions.UsernameAlreadyExistsException;
 import io.agileintelligence.ppmtool.repositories.UserRepository;
 
 @Service
@@ -20,10 +21,18 @@ public class UserService {
       newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
 
       //Username has to be unique (exception)
+      try{
+          newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+          //Username has to be unique (exception)
+          newUser.setUsername(newUser.getUsername());
+          // Make sure that password and confirmPassword match
+          // We don't persist or show the confirmPassword
+          return userRepository.save(newUser);
 
-        // Make sure that password and confirmPassword match
-        // We don't persist or show the confirmPassword
-      return userRepository.save(newUser);
+      }catch (Exception e){
+          throw new UsernameAlreadyExistsException("Username '"+newUser.getUsername()+"' already exists");
+      }
+
     }
 
 }
