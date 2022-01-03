59aef81111b1eed3a5464d9a6ec5fcefe8df5602
mhall99 <erinmhall61@gmail.com>
2022-01-03 13:30:57 -0600

    JWT pre-work

--- ppmtool/ppmtool/src/main/java/io/agileintelligence/ppmtool/repositories/UserRepository.java
+++ ppmtool/ppmtool/src/main/java/io/agileintelligence/ppmtool/repositories/UserRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import io.agileintelligence.ppmtool.domain.User;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long>{
+	
+	 User findByUsername(String username);
+	 User getById(Long id);
 
 }
